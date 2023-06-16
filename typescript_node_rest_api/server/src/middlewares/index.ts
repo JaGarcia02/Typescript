0e8f16c0c72bce5a201ae766a551eaf83df88d25
JaGarcia02 <jagarcia02051998@gmail.com>
2023-06-16 16:06:26 +0800

    middleware done for auth

---
+++ typescript_node_rest_api/server/src/middlewares/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { get, merge } from "lodash";
+import { getUserBySessionToken } from "models/user_models";
+
+export const isAuthenticated = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const sessionToken = req.cookies["JA-AUTH"];
+    if (!sessionToken) {
+      return res.sendStatus(400);
+    }
+    const existingUser = await getUserBySessionToken(sessionToken);
+
+    if (!existingUser) {
+      return res.sendStatus(403);
+    }
+    merge(req, { identity: existingUser });
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};

7ae1fe29cefaa7b715ddb523e01cad90eed2c2e9
JaGarcia02 <jagarcia02051998@gmail.com>
2023-06-16 13:36:34 +0800

    this is my api ts tutorial

---
+++ typescript_node_rest_api/server/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server port: ${PORT}`);
+  console.log(`Server link: http://localhost:${PORT}/`);
+});

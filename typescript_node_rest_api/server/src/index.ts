2baa758c53c2b19bf37c8ca0992e15765b483ae5
JaGarcia02 <jagarcia02051998@gmail.com>
2023-06-16 15:24:50 +0800

    ts controller routes models

--- typescript_node_rest_api/server/src/index.ts
+++ typescript_node_rest_api/server/src/index.ts
@@ -4,8 +4,11 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
+
+import router from "./routers";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -26,3 +29,9 @@ server.listen(PORT, () => {
   console.log(`Server port: ${PORT}`);
   console.log(`Server link: http://localhost:${PORT}/`);
 });
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use("/", router());

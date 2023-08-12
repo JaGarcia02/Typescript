0c3786cc13db2826db6379a0ba3f08590bc2961b
Ja Garcia <jagarcia02051998@gmail.com>
2023-08-12 22:22:00 +0800

    ts postgres tutorial done

---
+++ typescript_sequelized/backend/seeders/projectAssignments.ts
@@ -0,0 +1,14 @@
+export const projectAssignments = [
+  {
+    ProjectId: "1",
+    UserId: "abd0758d-0c8c-44a5-8e73-b8aaf8fc8e6b",
+  },
+  {
+    ProjectId: "2",
+    UserId: "abd0758d-0c8c-44a5-8e73-b8aaf8fc8e6b",
+  },
+  {
+    ProjectId: "3",
+    UserId: "28f391d1-0758-4b81-9512-3f1202e6ef62",
+  },
+];
--- typescript_sequelized/backend/server.ts
+++ typescript_sequelized/backend/server.ts
@@ -4,20 +4,41 @@ import dotenv from "dotenv";
 dotenv.config();
 import { users } from "./seeders/users";
 import { projects } from "./seeders/project";
+import { projectAssignments } from "./seeders/projectAssignments";
 
 const app = expres();
 const port = process.env.PORT || 9999;
 
 // function create
 
-const Create_Project = () => {
-  projects.map((project) => db.Project.create(project));
-};
-Create_Project();
+// const Create_ProjectAssignments = () => {
+//   projectAssignments.map((projectAssign) =>
+//     db.ProjectAssignment.create(projectAssign)
+//   );
+// };
+// Create_ProjectAssignments();
+// const Create_Project = () => {
+//   projects.map((project) => db.Project.create(project));
+// };
+// Create_Project();
 // const Create_Users = () => {
 //   users.map((user) => db.User.create(user));
 // };
 // Create_Users();
+
+app.get("/", (req, res) => {
+  db.User.findAll({
+    include: {
+      model: db.Project,
+    },
+  })
+    .then((response: object) => {
+      res.status(200).json(response);
+    })
+    .catch((err: object) => {
+      res.status(500).json(err);
+    });
+});
 
 db.sequelize.sync().then(() => {
   app.listen(port, () => {

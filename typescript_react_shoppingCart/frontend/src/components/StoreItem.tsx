3a7cdc1428851b9020bd06d2fd3b4f3b6cee3a31
Ja Garcia <jagarcia02051998@gmail.com>
2023-09-06 20:51:28 +0800

    ts shopping cart update commit

---
+++ typescript_react_shoppingCart/frontend/src/components/StoreItem.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+type StoreItemProps = {
+  id: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+};
+import { Card } from "react-bootstrap";
+
+const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
+  return (
+    <>
+      <Card>
+        <Card.Img
+          variant="top"
+          src={imgUrl}
+          height="200px"
+          style={{ objectFit: "cover" }}
+        />
+        <Card.Body className="d-flex flex-colum">
+          <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+            <span className="fs-2">{name}</span>
+            <span className="ms-2 text-muted">{price}</span>
+          </Card.Title>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+export default StoreItem;
--- typescript_react_shoppingCart/frontend/src/pages/Store.tsx
+++ typescript_react_shoppingCart/frontend/src/pages/Store.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import storeItems from "../data/items.json";
+import { Row, Col } from "react-bootstrap";
+import StoreItem from "../components/StoreItem";
 
 const Store = () => {
-  return <div>Store</div>;
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => {
+          return (
+            <>
+              <Col key={item.id}>
+                <StoreItem {...item} />
+              </Col>
+            </>
+          );
+        })}
+      </Row>
+    </>
+  );
 };
 
 export default Store;

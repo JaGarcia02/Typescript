4c4be4b2f1906cf789e5d2decd307f746dbb2753
JaGarcia02 <jagarcia02051998@gmail.com>
2023-09-07 17:11:49 +0800

    react typescrtipt tutorial

--- typescript_freeCodeCamp-tutorail/client/src/components/TodoList.tsx
+++ typescript_freeCodeCamp-tutorail/client/src/components/TodoList.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { Todo } from "../models/todo_model";
+import SingleTodo from "./SingleTodo";
 interface TodoListProps {
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
 const TodoList: React.FC<TodoListProps> = ({ todos, setTodos }) => {
-  return <div className="todos"></div>;
+  return (
+    <div className="todos">
+      {todos.map((todo) => (
+        <>
+          <SingleTodo
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        </>
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;

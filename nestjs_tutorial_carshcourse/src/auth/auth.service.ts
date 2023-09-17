11a83dc6ce8eb784ecfbf76b4f8b66077096908d
Ja Garcia <jagarcia02051998@gmail.com>
2023-09-17 18:15:55 +0800

    update tutorial nestjs

--- nestjs_tutorial_carshcourse/src/auth/auth.service.ts
+++ nestjs_tutorial_carshcourse/src/auth/auth.service.ts
@@ -1,14 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { dot } from 'node:test/reporters';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    return 'I am signed up';
+  async signup(dto: AuthDto) {
+    //  Convert the password to hash
+    const hash = await argon.hash(dto.password);
+
+    try {
+      //  Save the new user to database
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+        },
+      });
+
+      //   remove the user password in the request
+      delete user.password;
+
+      // retutn the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email is already taken!');
+        }
+      }
+      throw new ForbiddenException(error);
+    }
   }
 
-  signin() {
-    return 'I am signed in';
+  async signin(dto: AuthDto) {
+    try {
+      // Find the user by email
+      const user = await this.prisma.user.findUnique({
+        where: { email: dto.email },
+      });
+
+      // if user don't exist throw error
+      if (!user) {
+        throw new ForbiddenException("Email dosen't exist!");
+      }
+
+      // compare password
+      const password_match = await argon.verify(user.password, dto.password);
+      // if password incorrect throw error
+      if (!password_match) {
+        throw new ForbiddenException('Wrong password!');
+      }
+
+      //   remove the user password in the request
+      delete user.password;
+
+      // send back the user
+      return user;
+    } catch (error) {
+      throw new ForbiddenException(error);
+    }
   }
 }

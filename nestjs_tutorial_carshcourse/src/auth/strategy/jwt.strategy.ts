44c7dd57af937a6f162c8b532e1f92f9c676db9b
Ja Garcia <jagarcia02051998@gmail.com>
2023-09-17 20:52:51 +0800

    nest js tutorial fix jwt strategy module

--- nestjs_tutorial_carshcourse/src/auth/strategy/jwt.strategy.ts
+++ nestjs_tutorial_carshcourse/src/auth/strategy/jwt.strategy.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
-@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFormRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_SECRET,
     });
   }

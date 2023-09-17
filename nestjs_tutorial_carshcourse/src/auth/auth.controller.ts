852c255ef11cfb79e9f02a35079b635325deb616
Ja Garcia <jagarcia02051998@gmail.com>
2023-09-17 17:16:48 +0800

    update tutorial nestjs

--- nestjs_tutorial_carshcourse/src/auth/auth.controller.ts
+++ nestjs_tutorial_carshcourse/src/auth/auth.controller.ts
@@ -1,17 +1,19 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Request } from 'express';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() dto: AuthDto) {
     return this.authService.signup();
   }
 
   @Post('signin')
   signin() {
-    return this.authService.signin();
+    return this.authService.signin;
   }
 }

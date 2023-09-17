32f6ff705af8d13e9427aad908f203be13d2bd1e
Ja Garcia <jagarcia02051998@gmail.com>
2023-09-17 19:46:28 +0800

    update tutorial nestjs

--- nestjs_tutorial_carshcourse/src/prisma/prisma.service.ts
+++ nestjs_tutorial_carshcourse/src/prisma/prisma.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
-@Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {

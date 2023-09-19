636bafad4d76cbd7c6cec93ec8746412344a8409
JaGarcia02 <jagarcia02051998@gmail.com>
2023-09-19 10:07:07 +0800

    ete testing for the nestjs tutorial

--- nestjs_tutorial_carshcourse/src/auth/auth.controller.ts
+++ nestjs_tutorial_carshcourse/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Request } from 'express';
 import { AuthDto } from './dto';
@@ -13,7 +13,7 @@ export class AuthController {
   signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
   }
-
+  @HttpCode(HttpStatus.OK) // <---- this can modify the status code of the request
   @Post('signin') // ---- auth/signin
   signin(@Body() dto: AuthDto) {
     return this.authService.signin(dto);

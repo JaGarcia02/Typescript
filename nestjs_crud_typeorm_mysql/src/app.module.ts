02eabd7ee80c9f892b5a741b1d243e3a603d5497
Ja Garcia <jagarcia02051998@gmail.com>
2023-09-20 21:29:59 +0800

    update project

--- nestjs_crud_typeorm_mysql/src/app.module.ts
+++ nestjs_crud_typeorm_mysql/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './typeorm/entities/User';
 import { UsersModule } from './users/users.module';
@@ -20,7 +18,7 @@ import { UsersModule } from './users/users.module';
     }),
     UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

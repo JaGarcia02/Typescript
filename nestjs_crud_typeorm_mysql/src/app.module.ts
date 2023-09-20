dac4e76fdb0994ee9e82e33cc516ccc5e91b52a8
JaGarcia02 <jagarcia02051998@gmail.com>
2023-09-20 16:05:32 +0800

    nestjs new practice

--- nestjs_crud_typeorm_mysql/src/app.module.ts
+++ nestjs_crud_typeorm_mysql/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule],
   controllers: [AppController],
   providers: [AppService],
 })
